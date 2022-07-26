08009e359b291ed6e50905b9ce3158f5effff0b7
jerry22lin <110014366+jerry22lin@users.noreply.github.com>
2022-07-26 14:11:07 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,113 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        `)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . # # . .
+        . # # . .
+        . # # . .
+        . # # . .
+        . # # . .
+        `)
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        `)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onGesture(Gesture.FreeFall, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . # .
+        . # . . #
+        # . # # .
+        . # . # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        # . . # .
+        . # # . #
+        # # . # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . # # .
+        . . # # .
+        . . # # .
+        . . # # .
+        . . # # .
+        `)
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        `)
+})
+basic.forever(function () {
+	
+})
